9f4360a8c9aac805a38defbfd489ca2c20ca010b
Malak <malakhumadhat@gmail.com>
2026-08-04 23:16:20 +0300

    Initial Commit

---
+++ SkyCast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SkyCast
+//
+//  Created by Malak on 04/08/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SkyCastTests/SkyCastTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkyCastTests.swift
+//  SkyCastTests
+//
+//  Created by Malak on 04/08/2026.
+//
+
+import Testing
+@testable import SkyCast
+
+struct SkyCastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
